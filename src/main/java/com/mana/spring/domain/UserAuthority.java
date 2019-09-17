ded7ed25ef64db41816236658476ce9bde28e192
nik8singh <nscoder8@gmail.com>
2019-09-16 23:20:08 -0400

    Product

---
+++ src/main/java/com/mana/spring/domain/UserAuthority.java
@@ -0,0 +1,61 @@
+package com.mana.spring.domain;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authority")
+public class UserAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "authorityId")
+    private long authorityId;
+
+    @Column(name = "role")
+    private String role;
+
+    @ManyToOne()
+    @JoinColumn(name = "authority_user")
+    @JsonIdentityInfo(
+            generator = ObjectIdGenerators.PropertyGenerator.class,
+            property = "userId")
+    private User user;
+
+    public UserAuthority() {
+    }
+
+    public UserAuthority(User user,boolean admin) {
+        this.user = user;
+        if (admin)
+            role = "ROLE_ADMIN";
+        else
+            role = "ROLE_CUSTOMER";
+    }
+
+    public long getAuthorityId() {
+        return authorityId;
+    }
+
+    public void setAuthorityId(long authorityId) {
+        this.authorityId = authorityId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/mana/spring/dto/ProductRepoFilter.java
@@ -0,0 +1,60 @@
+package com.mana.spring.dto;
+
+import com.mana.spring.domain.Gemstone;
+import com.mana.spring.domain.JewelryType;
+import com.mana.spring.domain.Metal;
+import com.mana.spring.util.Pagination;
+
+import java.util.Set;
+
+public class ProductRepoFilter extends Pagination {
+
+    private JewelryType productJewelryTypes;
+    private Set<Gemstone> productGemstones;
+    private Set<Metal> productMetals;
+    private String sortOrder;
+    private String sortByr;
+
+    public ProductRepoFilter() {
+    }
+
+    public JewelryType getProductJewelryTypes() {
+        return productJewelryTypes;
+    }
+
+    public void setProductJewelryTypes(JewelryType productJewelryTypes) {
+        this.productJewelryTypes = productJewelryTypes;
+    }
+
+    public Set<Gemstone> getProductGemstones() {
+        return productGemstones;
+    }
+
+    public void setProductGemstones(Set<Gemstone> productGemstones) {
+        this.productGemstones = productGemstones;
+    }
+
+    public Set<Metal> getProductMetals() {
+        return productMetals;
+    }
+
+    public void setProductMetals(Set<Metal> productMetals) {
+        this.productMetals = productMetals;
+    }
+
+    public String getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(String sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public String getSortByr() {
+        return sortByr;
+    }
+
+    public void setSortByr(String sortByr) {
+        this.sortByr = sortByr;
+    }
+}
